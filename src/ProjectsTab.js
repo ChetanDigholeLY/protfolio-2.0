4c18a84ee26a09303416d054a7635fd83cfed364
Chetan Dighole <113523391+ChetanDighole@users.noreply.github.com>
2024-11-03 00:03:26 +0530

    task master

--- src/ProjectsTab.js
+++ src/ProjectsTab.js
@@ -184,8 +184,8 @@ function ProjectsTab() {
                         </div>
                         <h1 className='font-[myFont] text-5xl font-medium'>Task Master</h1>
                         <p className='text-[#9E9E9E] text-xl text-center font-sans'>Task Master is a powerful productivity tool that lets you stay organized and on top of all your tasks and goals.</p>
-                        <div className='flex justify-center items-center w-full'>
-                            {/* <a href='https://task-master-cd.netlify.app/' target="_blank" rel="noopener noreferrer" className='pointer-cursor font-sans underline underline-offset-2'>Live</a> */}
+                        <div className='flex justify-between items-center w-full'>
+                            <a href='https://task-master-aee6.vercel.app/' target="_blank" rel="noopener noreferrer" className='pointer-cursor font-sans underline underline-offset-2'>Live</a>
                             <a href='https://github.com/ChetanDighole/Task-Master' target="_blank" rel="noopener noreferrer" className='pointer-cursor font-sans underline underline-offset-2'>Source code</a>
                         </div>
                     </div>
